684e7968410b407dee43e6ad18085cabbbc7bd47
AureKad <93446185+AureKad@users.noreply.github.com>
2024-06-24 00:44:46 +0200

    prod ready #1.3.0

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,14 +1,16 @@
-import { ViewportScroller } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Observable, Subscriber, Subscription } from 'rxjs';
+
+import { AfterViewInit, Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild, viewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, AfterViewInit {
+  navbarfixed = false;
+  @ViewChild('header', { read: ElementRef })headerView!:ElementRef;
+  headerHeight!: number;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -17,6 +19,18 @@ export class AboutComponent implements OnInit {
       const element = document.querySelector("#" + f)
       if (element) element.scrollIntoView()
     })
+  }
+
+  ngAfterViewInit(): void {
+    this.headerHeight = this.headerView.nativeElement.offsetHeight
+  }
+
+  @HostListener('window:scroll', []) onScroll() {
+    console.log(window.scrollY)
+    if (window.scrollY > this.headerHeight -1) 
+      this.navbarfixed = true
+    else 
+      this.navbarfixed = false
   }
 
 
